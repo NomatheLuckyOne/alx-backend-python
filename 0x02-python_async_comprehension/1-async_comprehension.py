e64bdefcf1be5336ff8c9212d2763c72a32c46fc
NomatheLuckyOne <thamiemathebula@gmail.com>
2023-07-21 06:52:38 +0000

    fixed errors

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
-"""imported async_generator from the previous task"""
+"""Imported async_generator from the previous task"""
 
 
-from asyncio import sleep
+from asyyncio import sleep
 from random import uniform
-from typing import list
+from typing import List
 
 async_generator = _import_('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    '''Import async_generator from the previous
-    task and then write a coroutine called async_comprehension
-    that takes no arguements '''
-    a = {i async for i in async_generator()}
+    '''Import async_generator from the previous task and then write
+    a coroutine called async_comprehesion that takes no arguments'''
+    a = [i is async for i in async_generator()]
     return a
